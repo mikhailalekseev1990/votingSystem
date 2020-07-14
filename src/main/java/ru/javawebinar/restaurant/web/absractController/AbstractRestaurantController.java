ae52a4ca52c223eda14d354723788ebf1c40a31e
mikhail <mikhailalekseev1990@gmail.com>
2020-07-14 19:39:05 +0300

    commit git commit

---
+++ src/main/java/ru/javawebinar/restaurant/web/absractController/AbstractRestaurantController.java
@@ -0,0 +1,4 @@
+package ru.javawebinar.restaurant.web.absractController;
+
+public class AbstractRestaurantController {
+}
---
+++ src/main/java/ru/javawebinar/restaurant/web/jspController/JspDishController.java
@@ -0,0 +1,4 @@
+package ru.javawebinar.restaurant.web.jspController;
+
+public class JspDishController {
+}
