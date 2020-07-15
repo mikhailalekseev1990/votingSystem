c09824e9c7f722497715a0f6cd698270f1803b31
mikhail <mikhailalekseev1990@gmail.com>
2020-07-15 11:32:51 +0300

    AbstractRestaurantController

--- src/main/java/ru/javawebinar/restaurant/SpringMain.java
+++ src/main/java/ru/javawebinar/restaurant/SpringMain.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 public class SpringMain {
     public static void main(String[] args) {
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
         }
 
--- src/main/java/ru/javawebinar/restaurant/web/absractController/AbstractRestaurantController.java
+++ src/main/java/ru/javawebinar/restaurant/web/absractController/AbstractRestaurantController.java
@@ -1,10 +1,53 @@
 package ru.javawebinar.restaurant.web.absractController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import ru.javawebinar.restaurant.model.Restaurant;
 import ru.javawebinar.restaurant.repository.DishRepository;
+import ru.javawebinar.restaurant.repository.RestaurantRepository;
+import ru.javawebinar.restaurant.web.SecurityUtil;
+
+import static ru.javawebinar.restaurant.Utils.ValidationUtil.*;
+
+import java.util.List;
 
 public abstract class AbstractRestaurantController {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractRestaurantController.class);
 
     @Autowired
-    DishRepository dishRepository;
+    RestaurantRepository restaurantRepository;
+
+    public Restaurant get(int id) {
+        int userId = SecurityUtil.authUserId();
+        LOG.info("get restaurant {} for user {}", id, userId);
+        return checkNotFoundWithId(restaurantRepository.get(id, userId), id);
+    }
+
+    public void delete(int id) {
+        int userId = SecurityUtil.authUserId();
+        LOG.info("delete restaurant {} for user {}", id, userId);
+        checkNotFoundWithId(restaurantRepository.delete(id, userId), id);
+    }
+
+    public Restaurant create(Restaurant restaurant) {
+        int userId = SecurityUtil.authUserId();
+        LOG.info("create restaurant for user {}", userId);
+        Assert.notNull(restaurant, "restaurant must not be null");
+        return restaurantRepository.save(restaurant, userId);
+    }
+
+    public void update(Restaurant restaurant, int id) {
+        int userId = SecurityUtil.authUserId();
+        LOG.info("update restaurant {} for user {}", id, userId);
+        Assert.notNull(restaurant, "restaurant must not be null");
+        checkNotFoundWithId(restaurantRepository.save(restaurant, userId), restaurant.id());
+    }
+
+    public List<Restaurant> getAll() {
+        int userId = SecurityUtil.authUserId();
+        LOG.info("getAll restaurants for user {}", userId);
+        return restaurantRepository.getAll(userId);
+    }
 }
